962118f9cc6bea88d67ceba13e02788aa5e181f6
tall-brownie <harshgolpark@gmail.com>
2025-08-22 20:49:52 +0530

    frontend

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/signup'
+import { Signin } from './pages/signin'
+import { Blog } from './pages/blog'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
-}
 
+}
 export default App
+    
